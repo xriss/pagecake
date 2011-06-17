31f0ef7dd49a0a5dac83e66a68ab5efa606837ad
kriss <devnull@localhost>
2011-06-17 15:08:27 +0100

    bootstrapp bumped to 20110617.58

--- apps/boot-str/bump.sh
+++ apps/boot-str/bump.sh
@@ -1,4 +1,4 @@
 ver=`lua bump.lua`
 hg ci -m "bootstrapp bumped to $ver"
 hg push
-ssh web "su wet -c\"cd hg/aelua;hg up;cd apps/boot-str;make;cd ../../php;tail log.txt;exit\""
+ssh web "su wet -c\"cd hg/aelua;hg pull;hg up;cd apps/boot-str;make;cd ../../php;tail log.txt;exit\""
