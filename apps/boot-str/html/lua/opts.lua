0aa6c4a3d0ebc2f32d30b6bf86f1d03b109dcd92
kriss <devnull@localhost>
2011-07-27 22:34:31 +0100

    bootstrapp bumped to 20110727.89

--- apps/boot-str/html/lua/opts.lua
+++ apps/boot-str/html/lua/opts.lua
@@ -18,7 +18,7 @@ local type=type
 module("opts")
 
 --Machine generated data. Read between the lines, but do not edit.
-bootstrapp_version=20110702.92
+bootstrapp_version=20110727.89
 --Machine generated data. Read between the lines, but do not edit.
 
 mail={}
