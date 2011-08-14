e2e563984500210dab0bdac042e857296b4f3327
kriss <devnull@localhost>
2011-08-14 20:33:04 +0100

    prep to write notes

--- import/do.lua
+++ import/do.lua
@@ -22,6 +22,8 @@ elseif cmd=="write" then
 
 	if form=="comic" then
 		dofile("do.write.comic.lua")
+	elseif form=="note" then
+		dofile("do.write.note.lua")
 	end
 	
 elseif cmd=="clear" then
@@ -34,8 +36,11 @@ Please choose one of the following to perform
 
 clear -- clear all data in the local cache do this at the start unless you want to merge sites
 read -- read data from a site into the local cache
+read 4lfa -- my comic data (obsolete)
+read fud -- my forum data, uses a fud forum xml feed
 write -- write data to a site from the local cache
-
+write comic -- upload some comics
+write note -- upload notes/comments
 ]])
 
 end
--- import/do.write.note.lua
+++ import/do.write.note.lua
@@ -7,7 +7,7 @@ if not dest or not sess then
 	put([[
 Must specify a site domain eg host.local:8080 and a wet_session with admin permission to upload from your current ip like so
 
-./do.lua write comic host.local:8080 0123456789abcdef0123456789abcdef
+./do.lua write note host.local:8080 0123456789abcdef0123456789abcdef
 
 ]])
 	return
@@ -80,35 +80,12 @@ function multiparts(data)
 end
 
 
-function upload_data(data)
+function upload_note(notename,data)
 
 	local req_body,boundary=multiparts(data)
 --put(req_body)
 	local res_body={}
-	local suc, headers, code = socket.http.request{
-		url="http://"..dest.."/data",
-		method="POST",
-		headers={
-					["Content-type"]="multipart/form-data; boundary="..boundary,
-					["Content-Length"] = #req_body,
-					["Cookie"]="wet_session="..sess,
-					["Referer"]="http://"..dest.."/data",
-				},
-		source = ltn12.source.string(req_body),
-		sink = ltn12.sink.table(res_body),
-	}
-	put("Received "..suc.." "..headers.." "..(#(code or "")).."\n")
---	table.foreach(res_body,print)
-
-end
-
-
-function upload_waka(wakaname,data)
-
-	local req_body,boundary=multiparts(data)
---put(req_body)
-	local res_body={}
-	local url="http://"..dest.."/?cmd=edit&page="..wakaname
+	local url="http://"..dest.."/note/api?cmd=edit&thread="..notename
 	local suc, headers, code = socket.http.request{
 		url=url,
 		method="POST",
@@ -126,89 +103,12 @@ function upload_waka(wakaname,data)
 
 end
 
-function b64enc(s)
-
-	local source=ltn12.source.string(s)
-
-	local ret={}
-	local filter=ltn12.filter.chain(
-	  mime.encode("base64"),
-	  mime.wrap("base64")
-	)
-	
-	local sink=ltn12.sink.chain(filter, ltn12.sink.table(ret) )
-	
-	ltn12.pump.all(source ,  sink)
-
-	return table.concat(ret)
-end
 
 
 
 
-	function dirdata()
-
-		local d="cache/data"
-		for id in lfs.dir(d) do
-			local fname=d.."/"..id
-			
-			local id1=tostring(id)
-			local id2=id1
-			
-			local ids=ssplit(".",id1,false)
-			
-			if #ids>1 then -- use extension as filename
-				id2="."..ids[#ids]
-				ids[#ids]=nil
-				id1=table.concat(ids,".")
-			end
-			
-			
-			
-			local a=lfs.attributes(fname)
-			
-			if a.mode=="file" then
-			
-				local dat=readfile(fname)
-
-put("Uploading /data/"..id1.."/"..id2.."\n")
-
-				upload_data({
-					submit={data="Upload"},
-					dataid={data=id1},
-					filename={data=id2},
-					mimetype={data=""},
-					filedata={mimetype="application/octet-stream;charset=utf-8",encoding="binary",data=dat,filename=id2},
-					})
-
-			
-			elseif a.mode=="directory" then -- a subdir upload
-
-				if id~="." and id~=".." then -- ignore these
-
-	--[[	
-				for vv in lfs.dir(fname) do
-				
-					local fname=d.."/"..id
-					
-					local a=lfs.attributes(fname)
-					
-					if a.mode=="file" then -- only one file
-
-						break
-					end
-				
-				end
-	]]
-				end
-			end
-		end
-	end
-	dirdata(d)
-
-
-	local based="cache/waka"
-	function dirwaka(d)
+	local based="cache/note"
+	function dirnote(d)
 	
 		for id in lfs.dir(d) do
 			local fname=d.."/"..id
@@ -228,40 +128,41 @@ put("Uploading /data/"..id1.."/"..id2.."\n")
 			
 			if a.mode=="file" then
 			
-				if id2==".txt" then
+				if id2==".json" then
 				
 					local dat=readfile(fname)
 					
-					local wakaname=(d.."/"..id1):sub(#based+2)
+					local threadname=(d.."/"..id1):sub(#based+2)
 					
-	put("Editing "..wakaname.."\n")
+put("Parsing "..threadname.." this may take a little while...\n")
+	
+					local threads=json.decode(dat).threads
+					
+					for i,v in pairs(threads) do
+					
+put("Sending thread "..tostring(v[1].uid).."\n")
 
+						for i,c in ipairs(v) do
+
+
+						end
+						
+					end
+
+--[[
 					upload_waka(wakaname,{
 						submit={data="Save"},
 						text={data=dat},
 						})
-						
+]]						
 				end
 				
 			elseif a.mode=="directory" then -- subdir
 			
 				if id~="." and id~=".." then -- ignore these
-					dirwaka(fname)
+					dirnote(fname)
 				end
 			end
 		end
 	end
-	dirwaka(based)
-
---[[
-
-	local wakaname="test"
-	local dat="Screw you hippy!"
-	put("Editing "..wakaname.."\n")
-
-	upload_waka(wakaname,{
-		submit={data="Save"},
-		text={data=dat},
-		})
-
-]]
+	dirnote(based)
