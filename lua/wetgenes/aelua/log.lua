1639b55bfa1b95ea9d75936f434dc06873ba05f6
kriss <devnull@localhost>
2011-12-27 14:42:43 +0000

    fized line endings

--- lua/wetgenes/aelua/log.lua
+++ lua/wetgenes/aelua/log.lua
@@ -1,1 +1,14 @@
-local core=require("wetgenes.aelua.log.core")module("wetgenes.aelua.log")function log(a,b)	return core.log(a,b)end
+
+
+local core=require("wetgenes.aelua.log.core")
+
+
+
+module("wetgenes.aelua.log")
+
+
+function log(a,b)
+
+	return core.log(a,b)
+
+end
--- lua/wetgenes/aelua/mail.lua
+++ lua/wetgenes/aelua/mail.lua
@@ -1,1 +1,12 @@
-local core=require("wetgenes.aelua.mail.core")local os=osmodule("wetgenes.aelua.mail")function send(...)	return core.send(...)end
+
+
+local core=require("wetgenes.aelua.mail.core")
+
+local os=os
+
+module("wetgenes.aelua.mail")
+
+
+function send(...)
+	return core.send(...)
+end
