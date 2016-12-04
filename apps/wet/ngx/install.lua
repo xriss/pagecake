cecb26be32dc39199d862312832e35be6bcd84bb
kriss <kriss@xixs.com>
2016-12-04 04:01:14 +0000

    install to old hg repo

--- apps/wet/ngx/install.lua
+++ apps/wet/ngx/install.lua
@@ -1,7 +1,7 @@
 -- build or serve an nginx version of an app
 
-local apps=require("apps")
-apps.setpaths(nil,{apps.find_bin()})
+local codepath=require("apps").default_paths() -- default search paths so things can easily be found
+--if codepath then print("Using code found at : "..codepath.."lua") end
 
 local bake_ngx=require("wetgenes.bake.ngx")
 
